beeacb2b3af8fc25b5823b3153bf347e63a31d5e
UK1\MinhNg <minhng@joblogic.com>
2024-08-05 17:46:13 +0700

    SMF-02 upgrade dependencies

--- makefiles/server.mk
+++ makefiles/server.mk
@@ -5,7 +5,7 @@ server.venv: ## Create a virtual environment
 	python -m venv venv && .\venv\Scripts\activate
 
 server.install: ## Install server with its dependencies
-	pip install -r requirements-dev.txt
+	pip install -r requirements-dev.txt --upgrade
 
 server.start: ## Start server in its docker container
 	python src/server.py
