37316aca961f402aa9e73573a77a15dbc3108553
UK1\MinhNg <minhng@joblogic.com>
2024-08-05 17:08:18 +0700

    SMF-01 initialize source

---
+++ makefiles/server.mk
@@ -0,0 +1,14 @@
+### SERVER
+# ¯¯¯¯¯¯¯¯¯¯¯
+
+server.venv: ## Create a virtual environment
+	python -m venv venv && .\venv\Scripts\activate
+
+server.install: ## Install server with its dependencies
+	pip install -r requirements-dev.txt
+
+server.start: ## Start server in its docker container
+	python src/server.py
+
+server.upgrade: ## Upgrade pip dependencies
+	bash -c "python vendor/bin/pip-upgrade requirements.txt requirements-dev.txt --skip-virtualenv-check"
