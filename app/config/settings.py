03658bc88d88f00ff0b2644c33271ee655658156
UK1\MinhNg <minhng@joblogic.com>
2024-08-19 15:51:37 +0700

    SMF-06 remove unused code

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,18 +6,9 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
-# REDIS_CONN_URL = os.environ.get("REDIS_CONN_URL")
-
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:123@localhost:5432/slot")
 
-
 SEVEN_PROBABILITY = os.environ.get("SEVEN_PROBABILITY", 0.02)
-#
-# SOCKET_IO_EMITTER_KEY = os.environ.get("SOCKET_IO_EMITTER_KEY")
-# REDIS_PUB_SUB_CREATE_USER_CHAT_APP = os.environ.get(
-#     "REDIS_PUB_SUB_CREATE_USER_CHAT_APP"
-# )
-
 
 JWT_PUBLIC_KEY_PATH = os.environ.get("JWT_PUBLIC_KEY_PATH", default="jwt_api_key.pub")
 JWT_PUBLIC_KEY = open(JWT_PUBLIC_KEY_PATH).read()
