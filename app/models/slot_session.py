cb679c57ae20c636ecf15c4a3510355b2aceabf3
UK1\MinhNg <minhng@joblogic.com>
2024-08-23 15:56:10 +0700

    SMP-12 update models

--- app/models/slot_session.py
+++ app/models/slot_session.py
@@ -1,5 +1,3 @@
-from uuid import uuid4
-
 from app.config.database import Base
 from sqlalchemy import (
     JSON,
@@ -12,9 +10,9 @@ from sqlalchemy.orm import relationship
 
 
 class SlotSession(Base):
-    id = Column(String, primary_key=True, default=uuid4)
+    id = Column(Integer, primary_key=True)
     result = Column(JSON, nullable=True)
     bet = Column(Integer, nullable=True)
     change = Column(Integer, nullable=True)
-    user_id = Column(String, ForeignKey("user.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
     user = relationship("User", back_populates="slot_sessions")
